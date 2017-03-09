3ae57a9f4fc1a5c0e5fef64f57db0e90a847676f
Axdliu <axdliu@gmail.com>
2017-03-08 20:37:58 -0500

    update

--- ndarray_basics.py
+++ ndarray_basics.py
@@ -45,3 +45,16 @@ I = 5000
 
 %time mat = np.random.standard_normal((I, I))
 %time mat.sum()
+
+dt = np.dtype([('Name', 'S10'), ('Age', 'i4'), \
+               ('Height', 'f'), ('Children/Pets', 'i4', 2)])
+s = np.array([('Smith', 45, 1.83, (0, 1)), \
+              ('Jones', 53, 1.72, (2, 2))], dtype=dt)
+print s
+
+
+x = np.random.standard_normal((5, 10000000))
+y = 2 * x + 3 # linear equation y = a * x + b
+C = np.array((x, y), order='C')
+F = np.array((x, y), order='F')
+x = 0.0; y = 0.0 # memory cleanup
